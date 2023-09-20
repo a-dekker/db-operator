1faab56a3ba20f9c8404104dfb509d67f74098fe
Nikolai Rodionov <allanger@zohomail.com>
2023-09-20 22:12:26 +0200

    WIP: Fixing code after the review X2
    Remove templates from dbuser

--- api/v1beta1/dbuser_types.go
+++ api/v1beta1/dbuser_types.go
@@ -32,8 +32,6 @@ type DbUserSpec struct {
 	AccessType string `json:"accessType"`
 	// SecretName name that should be used to save user's credentials
 	SecretName string `json:"secretName"`
-	// Templates to add custom entries to ConfigMaps and Secrets
-	Templates Templates `json:"templates,omitempty"`
 }
 
 // DbUserStatus defines the observed state of DbUser
